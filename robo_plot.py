1ab685bc3be9c0f25bd77f8653f905a30cd190e5
Vebjørn <vb.bjorhovde@gmail.com>
2020-10-29 14:47:03 +0100

    Add welding gun

--- robo_plot.py
+++ robo_plot.py
@@ -4,6 +4,8 @@ import modern_robotics as mr
 from mpl_toolkits import mplot3d
 
 
+
+
 #Each joint will be defined by a point, this is also the case for the 90deg angles that are present in the robot.
 def robo_plot(thetalist):
     S = np.array([[0, 0 ,1, 0, 0, 0],
@@ -40,15 +42,28 @@ def robo_plot(thetalist):
                    [0, 0, 1, 0.505+0.760+0.200],
                    [0, 0, 0, 1]])
     
-    joint_frames = [M1, M2, M3, M4, M5, M6]
-    trans_mat = [M1, M2, M3, M4, M5, M6]
+    M7 = np.array([[1, 0, 0, 0.150+0.238+0.557+0.1],
+                   [0, 1, 0, 0],
+                   [0, 0, 1, 0.505+0.760+0.200],
+                   [0, 0, 0, 1]])
+    
+    joint_frames = [M1, M2, M3, M4, M5, M6, M7] #M1 - M& is a frame attached to each joint, M7 is the end effector frame
+    trans_mat = [M1, M2, M3, M4, M5, M6, M7]
     exp_trans = np.identity(4)
 
     for i in range(len(S)):
         trans_mat[i] = exp_trans@joint_frames[i]
-        exp_trans = exp_trans@mr.MatrixExp6(mr.VecTose3(S[:,i]*thetalist[i])) 
+        exp_trans = exp_trans@mr.MatrixExp6(mr.VecTose3(S[:,i]*thetalist[i]))
+        if i == range(len(S))[-1]:
+            trans_mat[i+1] = exp_trans@joint_frames[i+1] 
 
+    Tr = np.radians(54)
+    T = np.array([[np.cos(Tr), 0, np.sin(Tr), 0.450], #The transformation matrix between the end frame and the tool-tip
+                  [0, 1 ,0, 0],
+                  [-np.sin(Tr), 0, np.cos(Tr), -0.084],
+                  [0, 0, 0, 1]])
 
+    trans_mat[-1] = trans_mat[-1]@T
 
     #Garther the x,y,z coorinates in arrays
     x_list = np.zeros(len(trans_mat))
@@ -62,8 +77,17 @@ def robo_plot(thetalist):
 
     #Plot the points in a 3d-coordinate system
     ax = plt.axes(projection='3d')
-    ax.plot(x_list, y_list ,z_list ,'-') #Plot the robot links
-    ax.plot(x_list, y_list, z_list, 'r.') #Plot the robot joints
+    plt.title('YASKAWA Motoman GP25')
+    ax.plot(x_list, y_list ,z_list ,'-k') #Plot the robot links
+    ax.plot(x_list[0:len(x_list)-1], y_list[0:len(y_list)-1], z_list[0:len(z_list)-1        ], 'c.') #Plot the robot joints
+    color_list= ['-b', '-r', '-g']
+    c_scale = 0.3 #Used to scale the size of the coordinate axis
+    I = np.identity(3)*c_scale 
+    for i in range(3): #Plots coordinate axis for the base frame and end effector frame
+        ax.plot([0, I[0][i]], [0, I[1][i]], [0, I[2][i]], color_list[i])
+        ax.plot([x_list[-1], x_list[-1] + trans_mat[-1][0][i]*c_scale], [y_list[-1], y_list[-1] + trans_mat[-1][1][i]*c_scale], [z_list[-1], z_list[-1] + trans_mat[-1][2][i]*c_scale], color_list[i])
+
+
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
